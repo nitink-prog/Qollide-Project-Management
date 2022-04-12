84a65b63e15ea497734a1228e5f32c8fccad0302
Nitin K <80429955+nitink-prog@users.noreply.github.com>
2022-04-12 19:46:44 -0400

    added pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Create from "./pages/create/Create";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Login from "./pages/login/Login";
+import Project from "./pages/project/Project";
+import Signup from "./pages/signup/Signup";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
