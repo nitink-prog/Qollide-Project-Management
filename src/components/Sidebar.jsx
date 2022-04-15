a7e3b0bc249c046fd5fbf8074606afcf830db900
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-04-15 17:04:44 -0400

    show avatar on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
+import Avatar from "./Avatar";
 import "./Sidebar.css";
 import DashIcon from "../assets/dashboard_icon.svg";
 import AddIcon from "../assets/add_icon.svg";
@@ -10,29 +11,33 @@ export default function Sidebar() {
     <div className="sidebar">
       <div className="sidebar-content">
         <div className="user">
-          {/* User Icon and Username */}
           {user ? (
-            <p>Good Afternoon, {user.displayName}.</p>
+            <>
+              {user.photoURL && <Avatar src={user.photoURL} />}
+              <p>Good Afternoon, {user.displayName}.</p>
+            </>
           ) : (
             <p>Let's Get Qrackin'!</p>
           )}
         </div>
-        <nav className="links">
-          <ul>
-            <li>
-              <NavLink exact to="/">
-                <img src={DashIcon} alt="Dashboard Icon Button" />
-                <span>Dashboard</span>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/create">
-                <img src={AddIcon} alt="Add Icon Button" />
-                <span>New Project</span>
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
+        {user && (
+          <nav className="links">
+            <ul>
+              <li>
+                <NavLink exact to="/">
+                  <img src={DashIcon} alt="Dashboard Icon Button" />
+                  <span>Dashboard</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/create">
+                  <img src={AddIcon} alt="Add Icon Button" />
+                  <span>New Project</span>
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        )}
       </div>
     </div>
   );
