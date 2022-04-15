6f01836c6c319cb7f59554dfab78492597e73b8e
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-04-15 13:09:51 -0400

    route protection based on user status

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Create from "./pages/create/Create";
@@ -6,34 +6,39 @@ import Dashboard from "./pages/dashboard/Dashboard";
 import Login from "./pages/login/Login";
 import Project from "./pages/project/Project";
 import Signup from "./pages/signup/Signup";
+import { useAuthContext } from "./hooks/useAuthContext";
 import "./App.css";
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Sidebar />
-        <div className="container">
-          <Navbar />
-          <Switch>
-            <Route exact path="/">
-              <Dashboard />
-            </Route>
-            <Route path="/create">
-              <Create />
-            </Route>
-            <Route path="/projects/:id">
-              <Project />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/signup">
-              <Signup />
-            </Route>
-          </Switch>
-        </div>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Sidebar />
+          <div className="container">
+            <Navbar />
+            <Switch>
+              <Route exact path="/">
+                {user ? <Dashboard /> : <Redirect to="/login" />}
+              </Route>
+              <Route path="/create">
+                {user ? <Create /> : <Redirect to="/login" />}
+              </Route>
+              <Route path="/projects/:id">
+                {user ? <Project /> : <Redirect to="/login" />}
+              </Route>
+              <Route path="/login">
+                {user ? <Redirect to="/" /> : <Login />}
+              </Route>
+              <Route path="/signup">
+                {user ? <Redirect to="/" /> : <Signup />}
+              </Route>
+            </Switch>
+          </div>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
