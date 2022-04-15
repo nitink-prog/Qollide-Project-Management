9e927d61c2f43d45d9b39e432f987337a3e91345
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-04-15 17:04:17 -0400

    create userlist component to view all online

---
+++ src/components/UserList.jsx
@@ -0,0 +1,21 @@
+import { useCollection } from "../hooks/useCollection";
+import Avatar from "./Avatar";
+import "./UserList.css";
+
+export default function UserList() {
+  const { error, documents } = useCollection("users");
+  return (
+    <div className="user-list">
+      <h2>Users:</h2>
+      {error && <div className="error">{error}</div>}
+      {documents &&
+        documents.map((user) => (
+          <div key={user.id} className="user-list-item">
+            {user.online && <span className="online-user"></span>}
+            <span>{user.displayName}</span>
+            <Avatar src={user.photoURL} />
+          </div>
+        ))}
+    </div>
+  );
+}
