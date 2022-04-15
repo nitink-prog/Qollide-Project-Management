0428a5a334c61ef4586063589d38839e37523a3f
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-04-15 13:10:26 -0400

    log in works now

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,49 @@
-import "./Login.css"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useLogin } from "../../hooks/useLogin";
+import "./Login.css";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { error, isPending, login } = useLogin();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Welcome back!</h2>
+      <p>Please sign in.</p>
+      <Link to="/signup">Don't have an account?</Link>
+      <label>
+        <span>Email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      {isPending ? (
+        <button className="btn" disabled>
+          Loading...
+        </button>
+      ) : (
+        <button className="btn">Qollide!</button>
+      )}
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
 }
