91d02d40643bc8de553fcafd4618b82aab8f6b0a
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-04-15 17:03:50 -0400

    flesh out New Project tab

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,7 +1,59 @@
-import "./Create.css"
+import { useState } from "react";
+import "./Create.css";
 
 export default function Create() {
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [assignedUsers, setAssignedUsers] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, details, dueDate);
+  };
+
   return (
-    <div>Create</div>
-  )
+    <div className="create-form">
+      <h2 className="page-title">Hop into a New Project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project Name:</span>
+          <input
+            required
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Project Details:</span>
+          <textarea
+            required
+            type="text"
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
+          />
+        </label>
+        <label>
+          <span>Due Date:</span>
+          <input
+            required
+            type="date"
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+          />
+        </label>
+        <label>
+          <span>Category:</span>
+          {/* category selector here */}
+        </label>
+        <label>
+          <span>Qollide with:</span>
+          {/* assignedUsers selector here */}
+        </label>
+        <button className="btn">Let's Go!</button>
+      </form>
+    </div>
+  );
 }
