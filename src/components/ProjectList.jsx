3dc7638bb1e25f00b76a81296b0390726006c812
Nitin K <80429955+nitink-prog@users.noreply.github.com>
2022-04-20 19:11:38 -0400

    projects now display on dashboard and are styled

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,11 +1,26 @@
+import { Link } from "react-router-dom";
+import Avatar from "./Avatar";
+
 import "./ProjectList.css";
 
 export default function ProjectList({ projects }) {
   return (
-    <div>
+    <div className="project-list">
       {projects.length === 0 && <p>No projects yet...</p>}
       {projects.map((project) => (
-        <div key={project.id}>{project.name}</div>
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due on {project.dueDate.toDate().toDateString()}</p>
+          <div className="assigned-to">
+            <ul>
+              {project.assignedUsersList.map((user) => (
+                <li key={user.id}>
+                  <Avatar src={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
       ))}
     </div>
   );
