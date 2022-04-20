a67e78c384c4efbd41f61706280a58c2a7342d66
Nitin K <80429955+nitink-prog@users.noreply.github.com>
2022-04-19 21:29:37 -0400

    creating projectlist component to display projects

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,16 @@
-import "./Dashboard.css"
+import ProjectList from "../../components/ProjectList";
+import { useCollection } from "../../hooks/useCollection";
+
+import "./Dashboard.css";
 
 export default function Dashboard() {
+  const { documents, error } = useCollection("projects");
+
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
+  );
 }
